c909386353ab86629756a0a89fe1230ac77573ba
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-05-26 18:04:44 +0200

    first init

---
+++ vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a :href="github" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a :href="twitter" target="_blank" rel="noopener noreferrer">
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderBarLinks',
+  data() {
+    return {
+      github: 'https://github.com/mohammedlaslaa',
+      twitter: 'https://www.twitter.com/mohammedlaslaa',
+    };
+  },
+};
+</script>
